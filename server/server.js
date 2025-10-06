e90bf38f8fbf2c1b93ef1e8577bcf602415c99f1
DevBilal17 <muhammadbilalmb0312@gmail.com>
2025-10-06 15:19:55 +0500

    Use express.json below the webhook

--- server/server.js
+++ server/server.js
@@ -11,12 +11,12 @@ const app = express()
 await connectDB()
 
 // Initialize Middlewares
-app.use(express.json())
 app.use(cors())
 
 // Routes Middlewares
 app.use('/api/user',userRoutes)
 
+app.use(express.json())
 
 // Api Routes
 app.get('/',(req,res)=>{
